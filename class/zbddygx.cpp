c7248871865e094ca8a93ca6070ba842a1f6e956
abdtyx <1507844577@qq.com>
2021-11-07 15:24:44 +0800

    2021/11/7 try

---
+++ class/zbddygx.cpp
@@ -0,0 +1,15 @@
+// Table1 kcmbybyyqzbddygx
+#include <bits/stdc++.h>
+#include "matrix"
+
+
+// typedef pair
+
+
+class zbdgxjz {
+	public:
+		vector<string> axis_zb;
+		vector<string> axis_mb;
+		zbdgxjz();
+		~zbdgxjz();
+};
